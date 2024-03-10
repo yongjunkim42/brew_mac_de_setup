f2deb28417a4058783012192d9fcff9b9b4b6c8c
yongjunkim42 <36705812+yongjunkim42@users.noreply.github.com>
2024-03-10 16:21:26 -0400

    Initial Commit: Added .gitignore, mac_setup.sh, and README.md, and LICENSE

---
+++ README.md
@@ -0,0 +1,12 @@
+# Brew Mac Setup
+This git repo presents a shell script that will install frequently used development free tools for a data engineers.
+
+By Following through the shell script, your mac book will have the basic tools to start a data engineer environment
+
+## License
+Copyright (c) 2024, Yong Jun Kim
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree. 
+
---
+++ mac_setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Check if Homebrew is installed, if not, install it
+if ! command -v brew &> /dev/null; then
+    echo "Homebrew is not installed. Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Install required applications
+brew install --cask bitwarden
+brew install --cask google-chrome
+brew install --cask rectangle
+brew install --cask github
+brew install --cask microsoft-onenote
+brew install --cask docker
+brew install --cask visual-studio-code
+brew install --cask zoom
+brew install --cask spotify
+brew install --cask pycharm-ce
+brew install --cask dbeaver-community
+
+# Setup Xcode Command Line Tools. 
+# This allows you to run git commands in terminal
+xcode-select --install
+
+echo "Setup complete."
